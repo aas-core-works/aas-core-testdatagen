d6d69db3d26bb93cb34ad81980ae8c3a04c80a2e
Marko Ristin-Kaufmann <marko@ristin.ch>
2026-03-12 21:34:45 +0100

    Release 0.0.4
    * Fix examples for subnormal and normal xs:doubles (#5)

--- aas_core_testdatagen/__init__.py
+++ aas_core_testdatagen/__init__.py
@@ -1,6 +1,6 @@
 """Generate test data for AAS elements based on the meta-model."""
 
-__version__ = "0.0.1"
+__version__ = "0.0.4"
 __author__ = "Marko Ristin"
 __license__ = "License :: OSI Approved :: MIT License"
 __status__ = "Production/Stable"
